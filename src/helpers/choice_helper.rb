2ad24aec734ec0e6bdd30865b958e3d507d7cbad
Ray Myers <ray.myers@gmail.com>
2010-06-07 13:45:23 -0700

    Refactoring

--- src/helpers/choice_helper.rb
+++ src/helpers/choice_helper.rb
@@ -1,18 +1,18 @@
 module ChoiceHelper
   def phonemes()
     [
-    {:symbol => "i", :word => "FLEECE"},
-    {:symbol => "u", :word => "GOOSE"},
-    {:symbol => "ɪ", :word => "KIT"},
-    {:symbol => "ʊ", :word => "FOOT"},
-    {:symbol => "e", :word => "FACE"},
-    {:symbol => "ɚ", :word => "NURSE"},
-    {:symbol => "o", :word => "GOAT"},
-    {:symbol => "ɛ", :word => "DRESS"},
-    {:symbol => "ʌ", :word => "STRUT"},
-    {:symbol => "ɔ", :word => "THOUGHT"},
-    {:symbol => "æ", :word => "TRAP"},
-    {:symbol => "ɑ", :word => "LOT"}
+    {:symbol => "i", :word => "Fleece"},
+    {:symbol => "u", :word => "Goose"},
+    {:symbol => "ɪ", :word => "Kit"},
+    {:symbol => "ʊ", :word => "Foot"},
+    {:symbol => "e", :word => "Face"},
+    {:symbol => "ɚ", :word => "Nurse"},
+    {:symbol => "o", :word => "Goat"},
+    {:symbol => "ɛ", :word => "Dress"},
+    {:symbol => "ʌ", :word => "Strut"},
+    {:symbol => "ɔ", :word => "Thought"},
+    {:symbol => "æ", :word => "Trap"},
+    {:symbol => "ɑ", :word => "Lot"}
     ]
   end
   
